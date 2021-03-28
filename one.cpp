c6b52c1f2570c20e15755c9b5a73b865c86bd3a9
Tushar Saxena <stushar345@gmail.com>
2021-03-28 12:56:28 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,30 +1,30 @@
 #include<bits/stdc++.h> 
 using namespace std; 
 
-int search(int arr[], int n, int x, int k) 
+int search(int arr[], int n, int element, int k) 
 { 
-	int i = 0; 
-	while (i < n) 
-	{ 
-		if (arr[i] == x) 
-			return i; 
+    int i = 0; 
+    while (i < n) 
+    { 
+        if (arr[i] == element) 
+            return i; 
+        int ans=abs(arr[i]-element)/k;
+        i = i + max(1, ans); 
+    } 
 
-		i = i + max(1, abs(arr[i]-x)/k); 
-	} 
-
-	
-	return -1; 
+    
+    return -1; 
 } 
 
 int main() 
 { 
-	int arr[] = {2, 4, 5, 7, 7, 6}; 
-	int x = 6; 
-	int k = 2; 
-	int n = sizeof(arr)/sizeof(arr[0]); 
-	int ans=search(arr, n, x, k);
-	if(ans == -1)
-	cout << "number is not present!"; 
-	else
-	cout << "Element " << x << " is present at index "<<ans ;
+    int arr[] = {2, 4, 5, 7, 7, 6}; 
+    int element = 6; 
+    int k = 2; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    int ans=search(arr,n,element,k);
+    if(ans == -1)
+    cout << "number is not present!"; 
+    else
+    cout << "Element " <<element<< " is present at index "<<ans ;
 } 
